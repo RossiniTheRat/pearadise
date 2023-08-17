2ec45fb5814dd6f5bee0229e0c01abd974db83c0
DimitriBoldon <dimitri@tiksdd.com>
2023-08-17 18:47:47 -0500

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+      entry: './src/index',
+      output: {
+        path: __dirname + '/browser',
+        filename: 'oauth2-client.min.js',
+        library: 'OAuth2Client',
+        libraryTarget: 'umd'
+      },
+  
+      resolve: {
+        extensions: ['.web.ts', '.web.js', '.ts', '.js', '.json'],
+        fallback: { 'crypto': false }
+      },
+  
+      devtool: 'source-map',
+  
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    }
+  ];
